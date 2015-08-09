c33f2bacd010a56f216cdea996a5477d3f78ddc5
Roger <li.rojie@gmail.com>
2015-08-09 17:30:51 -0400

    resume revamp
    revamped how the resume is displayed. current next fix: make web
    contents dynamic depending on window size AYOOOO

--- app.js
+++ app.js
@@ -2,47 +2,68 @@ var main = function() {
 
    $('.menu-icon').click(function(){
       if ($('.menu-icon').position().left === 0){
-         $('.menu').animate({left:"0px", duration:200, easing: "easeOutBounce"});
-         $('.non-hidden').find('*').show().animate({left: "250", duration: 200, easing: "easeOutBounce"});
+         $('.menu').animate({left:"0px"}, 500);
+         $('.non-hidden').find('*').show().animate({left: "250px"}, 500);
       }
       else {
-         $('.menu').animate({left: "-250", duration:200, easing:"easeOutBounce"});
-         $('.non-hidden *').animate({left: "0px", duration: 200, easing:"easeOutBounce"});
+         $('.menu').animate({left: "-250px"}, 500);
+         $('.non-hidden *').animate({left: "0px"}, 500);
+         $('.resume').animate({left: "-850px"}, 500);
       }
    });
 
-   $('.close-icon').click(function(){
-      $('.menu').animate({left: "-250", duration:200, easing:"easeOutBounce"});
-      $('.non-hidden *').animate({left: "0px", duration:200, easing:"easeOutBounce"});
-   });
+   $('#resume-button').click(function(){
+      if ($('.resume').position().left === -850){
+         $('.resume').animate({left: "0px"}, 500);
+         $('.non-hidden').find('*').show().animate({left:"850px"}, 500);
+         $('.menu').animate({left: "600px"}, 500);
+      }
+      else {
+         $('.resume').animate({left: "-600px"}, 500);
+         $('.non-hidden').find('*').show().animate({left:"250px"}, 500);
+         $('.menu').animate({left: "0px"}, 500);
+      }
+   })
 
    $('#home-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.non-hidden').offset().top}, 500);
+      $('.menu').animate({left: "-250px"}, 500);
+      $('.non-hidden *').animate({left: "0px"}, 500);
+      if ($('.resume').position().left === 0){
+         $('.resume').animate({left: "-850px"}, 500);
+      }
       });
 
    $('#about-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.about').offset().top}, 500);
+      $('.menu').animate({left: "-250px"}, 500);
+      $('.non-hidden *').animate({left: "0px"}, 500);
+      if ($('.resume').position().left === 0){
+         $('.resume').animate({left: "-850px"}, 500);
+      }
       });
 
-   $('#resume-button').click(function(){
+   $('#project-button').click(function(){
       $('html, body').animate({
-         scrollTop: $('.resume').offset().top}, 500);
-      });
-
-   $('#education-button').click(function(){
-      $('html, body').animate({
-         scrollTop: $('.education').offset().top}, 500);
+         scrollTop: $('.project').offset().top}, 500);
+      $('.menu').animate({left: "-250px"}, 500);
+      $('.non-hidden *').animate({left: "0px"}, 500);
+      if ($('.resume').position().left === 0){
+         $('.resume').animate({left: "-850px"}, 500);
+      }
       });
 
    $('#contact-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.contact').offset().top}, 500);
+      $('.menu').animate({left: "-250px"}, 500);
+      $('.non-hidden *').animate({left: "0px"}, 500);
+      if ($('.resume').position().left === 0){
+         $('.resume').animate({left: "-850px"}, 500);
+      }
       });
 
-   $('.resume-view').click(function(){
-      $('.resume-content').toggleClass('hide');
-   });
 }
 $(document).ready(main);

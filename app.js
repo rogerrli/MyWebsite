0e8bfe0b32206c48651f29fc4c1f4ade6d1e0228
Roger <li.rojie@gmail.com>
2015-08-09 20:25:09 -0400

    optimized things
    reaplaced all constants with %, page is now scalable or whatever. added
    resume tabs to switch between sections. future implemention: fix
    appearance (look sexy)

--- app.js
+++ app.js
@@ -2,68 +2,98 @@ var main = function() {
 
    $('.menu-icon').click(function(){
       if ($('.menu-icon').position().left === 0){
-         $('.menu').animate({left:"0px"}, 500);
-         $('.non-hidden').find('*').show().animate({left: "250px"}, 500);
+         $('.menu').animate({left:"0px"}, '20%');
+         $('.non-hidden').find('*').show().animate({left: "20%"}, '20%');
       }
       else {
-         $('.menu').animate({left: "-250px"}, 500);
-         $('.non-hidden *').animate({left: "0px"}, 500);
-         $('.resume').animate({left: "-850px"}, 500);
+         $('.menu').animate({left: "-20%"}, 500);
+         $('.non-hidden *').animate({left: "0%"}, 500);
+         $('.resume').animate({left: "-80%"}, 500);
       }
    });
 
    $('#resume-button').click(function(){
-      if ($('.resume').position().left === -850){
-         $('.resume').animate({left: "0px"}, 500);
-         $('.non-hidden').find('*').show().animate({left:"850px"}, 500);
-         $('.menu').animate({left: "600px"}, 500);
+      if ($('.menu').position().left === 0){
+         $('.resume').animate({left: "0%"}, 500);
+         $('.non-hidden').find('*').show().animate({left:"80%"}, 500);
+         $('.menu').animate({left: "60%"}, 500);
       }
       else {
-         $('.resume').animate({left: "-600px"}, 500);
-         $('.non-hidden').find('*').show().animate({left:"250px"}, 500);
-         $('.menu').animate({left: "0px"}, 500);
+         $('.resume').animate({left: "-60%"}, 500);
+         $('.non-hidden').find('*').show().animate({left:"20%"}, 500);
+         $('.menu').animate({left: "0%"}, 500);
       }
    })
 
    $('#home-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.non-hidden').offset().top}, 500);
-      $('.menu').animate({left: "-250px"}, 500);
+      $('.menu').animate({left: "-20%"}, 500);
       $('.non-hidden *').animate({left: "0px"}, 500);
       if ($('.resume').position().left === 0){
-         $('.resume').animate({left: "-850px"}, 500);
+         $('.resume').animate({left: "-80%"}, 500);
       }
-      });
+   });
 
    $('#about-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.about').offset().top}, 500);
-      $('.menu').animate({left: "-250px"}, 500);
+      $('.menu').animate({left: "-20%"}, 500);
       $('.non-hidden *').animate({left: "0px"}, 500);
       if ($('.resume').position().left === 0){
-         $('.resume').animate({left: "-850px"}, 500);
+         $('.resume').animate({left: "-80%"}, 500);
       }
-      });
+   });
 
    $('#project-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.project').offset().top}, 500);
-      $('.menu').animate({left: "-250px"}, 500);
+      $('.menu').animate({left: "-20%"}, 500);
       $('.non-hidden *').animate({left: "0px"}, 500);
       if ($('.resume').position().left === 0){
-         $('.resume').animate({left: "-850px"}, 500);
+         $('.resume').animate({left: "-80%"}, 500);
       }
-      });
+   });
 
    $('#contact-button').click(function(){
       $('html, body').animate({
          scrollTop: $('.contact').offset().top}, 500);
-      $('.menu').animate({left: "-250px"}, 500);
+      $('.menu').animate({left: "-20%"}, 500);
       $('.non-hidden *').animate({left: "0px"}, 500);
       if ($('.resume').position().left === 0){
-         $('.resume').animate({left: "-850px"}, 500);
+         $('.resume').animate({left: "-80%"}, 500);
       }
-      });
+   });
+
+   $('#education-tab').click(function(){
+      $('#education-content').removeClass('hide');
+      if($('#work-experience-content').hasClass('hide')){
+         $('#skills-content').addClass('hide');
+      }
+      else {
+         $('#work-experience-content').addClass('hide');
+      }
+   });
+
+   $('#work-experience-tab').click(function(){
+      $('#work-experience-content').removeClass('hide');
+      if($('#education-content').hasClass('hide')){
+         $('#skills-content').addClass('hide');
+      }
+      else {
+         $('#education-content').addClass('hide');
+      }
+   });
+
+   $('#skills-tab').click(function(){
+      $('#skills-content').removeClass('hide');
+      if($('#education-content').hasClass('hide')){
+         $('#work-experience-content').addClass('hide');
+      }
+      else {
+         $('#education-content').addClass('hide');
+      }
+   });
 
 }
 $(document).ready(main);
